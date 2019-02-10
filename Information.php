dd0f1535f2b7abfd20776ec78442ffa6f248e3e1
kande <saidouyoussouphk@gmail.com>
2019-02-10 18:02:51 +0000

    Changement du style

--- Information.php
+++ Information.php
@@ -6,28 +6,31 @@
 </head>
 <style type="text/css">
 	form{
-		background-color: lime ;
+		background-color: white;
         width: 50%;
 	}
     label{
     	text-align: center;
+    	text-align: none;
     }
     legend{
     	background-color: aqua;
     }
     footer{
     	text-align: center;
-    	background-color: lime;
+    	background-color: aqua;
     }
     body{
     	background-color: gray;
+    	background-image: url(hacking.jpg);
+    	background-repeat: no-repeat;
+		background-size: 105%;
     }
 
     img{
     	font-size: 100%;
      }
 
-    
 </style>
 <body>
 
@@ -38,10 +41,10 @@
 		<table>
 		<img src="téléchargé.png" width="15%" height="10%"><br><br><br>
 
-		<label for="Prénom"><small>Prénom: </small></label>
+		<label for="Prénom"><small>Prénom:</small></label>
 		<input type="text" name="Prénom" placeholder="Votre Prénom"><br><br><br>
 
-		<label for="Nom"><small>Nom: </small></label>
+		<label for="Nom"><small> Nom:</small></label>
 		<input type="text" name="Nom" placeholder="Votre Nom"><br><br><br>
 
 		<label for="Email"><small> Email: </small></label>
@@ -59,9 +62,9 @@
 		<input type="submit" name="Valider" align="rigth">
         </table>
 	</form></fieldset></div>
-	<footer width: 100%;> 
+	<footer width: 100% ><hr> 
 	<p>Pour plus d'informations contactez-nous sur</p>
-	<p>saidouyoussouphk@gmail.com    ou  le 77 667 49 07 </p><br>
+	<p>saidouyoussouphk@gmail.com    ou  le 77 667 49 07 </p><br><hr>
 	</footer>
 </body>
 </html>
